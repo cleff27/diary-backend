3c453d94bb64bd6e21d5def5d094d9906475a2ad
aadarsh <aadarshkaushik00@gmail.com>
2024-01-01 02:08:56 +0530

    updated cors URLs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://diary-anvu.onrender.com", "http://localhost:3000"],
     credentials: true,
   })
 );
